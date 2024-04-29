7d8f93c334dc1a681848d4a5cca56e886691e7a8
DILIP KUMAR BC <dilipkumarbc8055@gmail.com>
2024-04-29 17:32:23 +0530

    made changes in route

--- src/route/employee.route.js
+++ src/route/employee.route.js
@@ -9,7 +9,7 @@ employeeRouter.get("/signup", employee.signUp);
 employeeRouter.post("/signup", employee.postSignUp);
 employeeRouter.get("/login", employee.login);
 employeeRouter.post("/login", employee.postLogin);
-employeeRouter.get("/students/:id",employee.getStudentById);
+employeeRouter.get("/students/:id", jwtAuth, employee.getStudentById);
 employeeRouter.get("/student", jwtAuth, employee.studentform);
 employeeRouter.post("/students", jwtAuth, employee.postStudentform);
 employeeRouter.get("/companylist", jwtAuth, employee.companyList);
@@ -22,7 +22,5 @@ employeeRouter.get("/emphome", jwtAuth, employee.empHomepage);
 employeeRouter.get("/logout", jwtAuth, employee.logout);
 employeeRouter.get("/studentlist", jwtAuth, employee.studentList);
 employeeRouter.get("/downloadcsv", jwtAuth, employee.downloadCSV);
-
-
 
 export default employeeRouter;
