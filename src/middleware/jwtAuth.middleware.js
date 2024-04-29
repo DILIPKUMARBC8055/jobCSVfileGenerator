7a8e81b431a9750e1da747829e6cd65700064f7a
DILIP KUMAR BC <dilipkumarbc8055@gmail.com>
2024-04-29 17:28:17 +0530

    handling unauthorized

--- src/middleware/jwtAuth.middleware.js
+++ src/middleware/jwtAuth.middleware.js
@@ -5,28 +5,28 @@ import jwt from "jsonwebtoken";
 const jwtAuth = (req, res, next) => {
   // Extract token from cookies
   const token = req.cookies.token;
-  
+
   // Check if token exists
   if (token) {
     try {
       // Verify token validity
       const payload = jwt.verify(token, process.env.JWT_SECRET);
-      
+
       // If payload exists, set email in request and proceed to next middleware
       if (payload) {
         req.email = payload.email;
         next();
       } else {
         // If payload doesn't exist, send unauthorized response
-        return res.send("unauthorized");
+        return res.redirect("/");
       }
     } catch (error) {
       // If token verification fails, send unauthorized response
-      return res.send("unauthorized");
+      return res.redirect("/");
     }
   } else {
     // If no token is present, send unauthorized response
-    return res.send("unauthorized");
+    return res.redirect("/");
   }
 };
 
